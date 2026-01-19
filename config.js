26bb69d6a45ba97f794955242a177641ddc89d86
Alex <160778418+Alex000115@users.noreply.github.com>
2026-01-19 13:57:45 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Your deployed factory address
+    FACTORY_ADDRESS: "0xYourFactoryAddressHere",
+    
+    // Official EntryPoint (Same on all networks)
+    ENTRY_POINT_ADDRESS: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+};
